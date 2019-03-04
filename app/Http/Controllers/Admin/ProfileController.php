34086a7a3fda6881ebc72c2b6a14c190d54497b6
EC2 Default User <ec2-user@ip-172-31-8-91.us-east-2.compute.internal>
2019-03-04 15:28:49 +0000

    15

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,10 +14,26 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
-    }
+        \Debugbar::info($request);
+
+        // 以下を追記
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+    
+        $profile = new Profile;
+        $form = $request->all();
+    
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->gender = $request->radio('gender');
+        $profile->save();
+    
+        return redirect('admin/profile/create');    }
     
     public function edit()
     {
